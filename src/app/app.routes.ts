1990c81f1a7cce60b72b8341372317ea75fdff52
zainebyaich1 <zaineb.yaich@esprit.tn>
2024-04-13 11:42:48 +0100

    update crud reclamation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import {ProjetComponent} from "./Component/Projet/projet/projet.component";
 import {DossierComponent} from "./Component/Projet/dossier/dossier.component";
 import {LoginComponent} from "./Component/Projet/login/login.component";
 import {SignupComponent} from "./Component/Projet/signup/signup.component";
+import {ReclamationComponent} from "./Component/Reclamation/reclamation/reclamation.component";
+
 
 export const routes: Routes = [
   {path: 'projet', component :ProjetComponent},
   {path: 'dossier', component :DossierComponent},
   {path: 'login', component :LoginComponent},
   {path: 'signup', component :SignupComponent},
+  {path: 'reclamation', component :ReclamationComponent},
+
 
 ];
