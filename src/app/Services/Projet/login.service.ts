fffd9f9e0f149ad40d5473cb6be6492f409d1a33
FaresLoukil <fares.loukil@esprit.tn>
2024-04-13 02:51:54 +0100

    SignupCRUD+loginINTERFACE

---
+++ src/app/Services/Projet/login.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+const APIUrlLoginSignup = "http://localhost:8081/utilisateur";
+import {DataService} from "../data.service";
+import {HttpClient} from "@angular/common/http";
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService extends DataService{
+
+  constructor(http:HttpClient){
+    super(APIUrlLoginSignup,http);
+  }
+}
