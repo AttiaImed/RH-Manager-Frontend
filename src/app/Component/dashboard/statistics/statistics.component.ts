312626b23bd6cc6abe10c7e70ed618389d9869b5
zainebkhlifi1 <zaineb.khlifi@esprit.tn>
2024-05-15 17:36:02 +0100

    Revert "modification feedback"
    This reverts commit 6649a535

---
+++ src/app/Component/dashboard/statistics/statistics.component.ts
@@ -0,0 +1,154 @@
+import { Component } from '@angular/core';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { EChartsOption } from 'echarts';
+
+@Component({
+  selector: 'app-statistics',
+  standalone: true,
+  imports: [NgxEchartsDirective],
+  providers: [provideEcharts()],
+  templateUrl: './statistics.component.html',
+  styleUrl: './statistics.component.css',
+})
+export class StatisticsComponent {
+  // Data for charts
+  dataCircle = [
+    { value: 1048, name: 'Search Engine' },
+    { value: 735, name: 'Direct' },
+    { value: 580, name: 'Email' },
+    { value: 484, name: 'Union Ads' },
+    { value: 300, name: 'Video Ads' },
+  ];
+  dataCandlesHorizontale = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  dataCandles = [120, 200, 150, 80, 70, 110, 130];
+
+  // Charts Configurations
+  option: EChartsOption = {
+    tooltip: {
+      trigger: 'item',
+    },
+    legend: {
+      top: '5%',
+      left: 'center',
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        padAngle: 5,
+        itemStyle: {
+          borderRadius: 10,
+        },
+        label: {
+          show: false,
+          position: 'center',
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 40,
+            fontWeight: 'bold',
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+        data: this.dataCircle,
+      },
+    ],
+  };
+
+  option2: EChartsOption = {
+    xAxis: {
+      type: 'category',
+      data: this.dataCandlesHorizontale,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          backgroundColor: '#283b56',
+        },
+      },
+    },
+    toolbox: {
+      show: true,
+      feature: {
+        dataView: { readOnly: false },
+        restore: {},
+        saveAsImage: {},
+      },
+    },
+    series: [
+      {
+        data: this.dataCandles,
+        type: 'bar',
+      },
+    ],
+  };
+
+
+  optionzaineb: EChartsOption = {
+    xAxis: {
+      type: 'category',
+      boundaryGap: false
+    },
+    yAxis: {
+      type: 'value',
+      boundaryGap: [0, '30%']
+    },
+    visualMap: {
+      type: 'piecewise',
+      show: false,
+      dimension: 0,
+      seriesIndex: 0,
+      pieces: [
+        {
+          gt: 1,
+          lt: 3,
+          color: 'rgba(0, 0, 180, 0.4)'
+        },
+        {
+          gt: 5,
+          lt: 7,
+          color: 'rgba(0, 0, 180, 0.4)'
+        }
+      ]
+    },
+    series: [
+      {
+        type: 'line',
+        smooth: 0.6,
+        symbol: 'none',
+        lineStyle: {
+          color: '#5470C6',
+          width: 5
+        },
+        markLine: {
+          symbol: ['none', 'none'],
+          label: { show: false },
+          data: [{ xAxis: 1 }, { xAxis: 3 }, { xAxis: 5 }, { xAxis: 7 }]
+        },
+        areaStyle: {},
+        data: [
+          ['2019-10-10', 200],
+          ['2019-10-11', 560],
+          ['2019-10-12', 750],
+          ['2019-10-13', 580],
+          ['2019-10-14', 250],
+          ['2019-10-15', 300],
+          ['2019-10-16', 450],
+          ['2019-10-17', 300],
+          ['2019-10-18', 100]
+        ]
+      }
+    ],
+  };
+
+}
